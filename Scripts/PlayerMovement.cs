5d8b13cf62f7ed61290b1f9726526ef40655f631
Zackary Santana <zackzackyack@gmail.com>
2021-01-20 01:08:06 -0500

    Finalize animation and movement

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+
+    public Animator animator;
     public float maxSpeed, jumpHeight;
 
     void Update() {
+        animator.SetFloat("isMoving", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
         if (Input.GetKeyDown(KeyCode.W)) {
             // TODO: Jump
             if (Physics2D.IsTouchingLayers(GetComponent<Collider2D>())) {
@@ -17,18 +19,25 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if (Input.GetKey(KeyCode.A)) {
-            // TODO: Go LEFT
+            // Go left
             GetComponent<Rigidbody2D>().AddForce(new Vector2(-maxSpeed / 4, 0f), ForceMode2D.Force);
+
+            // Turn left
+            GetComponent<Rigidbody2D>().transform.rotation = new Quaternion(0, 180, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D)) {
-            // TODO: Go right
+            // Go right
             GetComponent<Rigidbody2D>().AddForce(new Vector2(maxSpeed / 4, 0f), ForceMode2D.Force);
+
+            // Turn right
+            GetComponent<Rigidbody2D>().transform.rotation = new Quaternion(0, 0, 0, 0);
         }
     }
 
     void FixedUpdate() {
         Rigidbody2D player = GetComponent<Rigidbody2D>();
+        // Capping the max speed
         if (player.velocity.x > maxSpeed) {
             player.velocity = new Vector2(maxSpeed, player.velocity.y);
         } else if (player.velocity.x < -maxSpeed) {
